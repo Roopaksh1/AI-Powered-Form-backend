c5b02770f52c90aa3d919afed6f78703ef81419b
Roopaksh <72032743+Roopaksh1@users.noreply.github.com>
2023-09-04 00:44:32 +0530

    change: add origin and credentials in cors

--- index.js
+++ index.js
@@ -5,14 +5,18 @@ const path = require('path');
 const logger = require('./src/utils/logger');
 const userRoutes = require('./src/api/v1/routes/userRoutes');
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:5173'],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
-app.use('/public',express.static(path.join(__dirname, 'public')));
 app.use('/v1/user/', userRoutes);
 
-app.listen(port = process.env.PORT, () => {
-    console.log(`listening to ${port}`);
-})
+app.listen((port = process.env.PORT), () => {
+  console.log(`listening to ${port}`);
+});
 
 logger.debug('server setup complete');
